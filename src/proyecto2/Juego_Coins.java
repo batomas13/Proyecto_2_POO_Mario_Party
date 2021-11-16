433490ef83fec399cbeb7fcadbe8f4ffcab9d28c
Adrian Ramirez Mattey <aramirezmattey@gmail.com>
2021-11-15 22:30:43 -0600

    Collect the Coins casi hecho

--- src/proyecto2/Juego_Coins.java
+++ src/proyecto2/Juego_Coins.java
@@ -5,6 +5,9 @@
  */
 package proyecto2;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
 /**
@@ -19,8 +22,9 @@ public class Juego_Coins extends javax.swing.JFrame {
     private final int BAD_COINS = 312;
     private int bad_count, good_count;
     */
-    public static final int BUTTON_SIZE = 25;
+    public static final int BUTTON_SIZE = 32;
     private JButton buttonArray[][] = new JButton[25][25];
+    public int puntaje = 0;
     
     
     /**
@@ -41,19 +45,39 @@ public class Juego_Coins extends javax.swing.JFrame {
     private void initComponents() {
 
         panelPrincipal = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        scoreField = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        changesArea = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        panelPrincipal.setPreferredSize(new java.awt.Dimension(800, 800));
+        panelPrincipal.setVerifyInputWhenFocusTarget(false);
 
         javax.swing.GroupLayout panelPrincipalLayout = new javax.swing.GroupLayout(panelPrincipal);
         panelPrincipal.setLayout(panelPrincipalLayout);
         panelPrincipalLayout.setHorizontalGroup(
             panelPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 750, Short.MAX_VALUE)
+            .addGap(0, 800, Short.MAX_VALUE)
         );
         panelPrincipalLayout.setVerticalGroup(
             panelPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 750, Short.MAX_VALUE)
+            .addGap(0, 800, Short.MAX_VALUE)
         );
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("PUNTOS");
+
+        scoreField.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
+        scoreField.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
+        scoreField.setText("0");
+        scoreField.setToolTipText("");
+
+        changesArea.setColumns(20);
+        changesArea.setRows(5);
+        jScrollPane1.setViewportView(changesArea);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -62,14 +86,28 @@ public class Juego_Coins extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(panelPrincipal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(scoreField, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addContainerGap(42, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(panelPrincipal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(panelPrincipal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(40, 40, 40)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(scoreField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(10, Short.MAX_VALUE))
         );
 
         pack();
@@ -114,15 +152,56 @@ public class Juego_Coins extends javax.swing.JFrame {
         int valorAleatorio;
         for (int i = 0; i < 25; i++) {
             for (int j = 0; j < 25; j++) {
+                // valor de la casilla
                 valorAleatorio = -10 + (int)(Math.random() * ((10 - -10) + 1));
                 tablero[i][j] = valorAleatorio;
-                System.out.print(tablero[i][j] + " ");
+                // inicializar el boton
+                buttonArray[i][j] = new JButton();
+                panelPrincipal.add(buttonArray[i][j]);
+                buttonArray[i][j].setBounds(j * BUTTON_SIZE, i * BUTTON_SIZE, BUTTON_SIZE, BUTTON_SIZE);
+                buttonArray[i][j].setBackground(new Color(230 + i, 220 + j, 25 + (j - i)));
+                buttonArray[i][j].addActionListener(listener);
+                
+                //System.out.print(tablero[i][j] + " ");
             }
-            System.out.println("");
+            //System.out.println("");
         }
     }
+    
+    public void updateScore() {
+        scoreField.setText(Integer.toString(puntaje));
+    }
+    
+    public void addHistory(int change) {
+        if (change >= 0) {
+            changesArea.append("+" + Integer.toString(change) + "\n");
+        }
+        else {
+            changesArea.append(Integer.toString(change) + "\n");
+        }
+    }
+    
+    ActionListener listener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int x, y;
+            if (e.getSource() instanceof JButton) {  
+                x = ((JButton) e.getSource()).getX() / BUTTON_SIZE;
+                y = ((JButton) e.getSource()).getY() / BUTTON_SIZE;
+                
+                puntaje += tablero[y][x];
+                updateScore();
+                addHistory(tablero[y][x]);
+                ((JButton) e.getSource()).setEnabled(false);
+            }
+        }
+    };
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextArea changesArea;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JPanel panelPrincipal;
+    private javax.swing.JTextField scoreField;
     // End of variables declaration//GEN-END:variables
 }
